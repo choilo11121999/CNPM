e070b891eb1252781f250ada9c7516181dffefd6
choilo11121999 <choilo11121999@gmail.com>
2020-05-12 11:06:28 +0700

    test.cpp changed

--- test.cpp
+++ test.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
-    cout << "hello";
+    int arr[5] = {};
 
+    cout << "Bai toan tim so nguyen duong lon nhat." << endl;
+    cout << "Nhap vao 5 so nguyen!" << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "So thu " << i+1 << ": ";
+        cin >> arr[i];
+    }
+
+    int max = 0;
+    for (int i = 0; i < 5; i++) {
+        if ( max < arr[i])
+            max = arr[i];
+    }
+
+    cout << "So nguyen duong lon nhat la: " << max;
     return 0;
 }
